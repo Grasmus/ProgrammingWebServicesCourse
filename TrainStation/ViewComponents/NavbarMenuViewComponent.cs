d00c07e7895971165e1cf881502d7eb3d2bb4542
vladyslav.sievostianov <vladyslav.sievostianov@stfalcon.com>
2023-12-20 23:22:52 +0200

    Add Navbar ViewComponent

---
+++ TrainStation/ViewComponents/NavbarMenuViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainStation.ViewComponents
+{
+    public class NavbarMenuViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
